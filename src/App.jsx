6e1cfc33cd38cc3ec3d96a348df1aa81e6c320cf
Blossom Ami <bloosmami@gmail.com>
2026-06-24 16:55:38 +0330

    Initialize React weather app with Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        Weather App
+      </h1>
+    </div>
+  )
+}
